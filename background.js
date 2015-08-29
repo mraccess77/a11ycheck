a4068651dfa596a13107f320f6db1ef5e3425ddd
mraccess77 <jon.avila@ssbbartgroup.com>
2015-08-28 23:12:46 -0400

    Added background image removal tool.  Rearranged items in conetxt menu.

--- background.js
+++ background.js
@@ -14,27 +14,22 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 chrome.contextMenus.create({
-	"title": "ARIA",
+	"title": "Show ARIA markup",
 	"contexts": ["all", "page", "selection", "image", "link"],
 	"onclick" : showARIA
 });
 
 chrome.contextMenus.create({
-	"title": "Darken",
-	"contexts": ["all", "page", "selection", "image", "link"],
-	"onclick" :  darken
-});
-
-chrome.contextMenus.create({
-	"title": "Enhanced Focus",
-	"contexts": ["all", "page", "selection", "image", "link"],
-	"onclick" :  enhancedFocus
-});
-
-chrome.contextMenus.create({
-	"title": "Remove Styles",
+	"title": "Remove styles",
 	"contexts": ["all", "page", "selection", "image", "link"],
 	"onclick" :  removeStyles
+});
+
+
+chrome.contextMenus.create({
+	"title": "Remove background images",
+	"contexts": ["all", "page", "selection", "image", "link"],
+	"onclick" :  removeBackgroundImages
 });
 
 chrome.contextMenus.create({
@@ -44,7 +39,7 @@ chrome.contextMenus.create({
 });
 
 chrome.contextMenus.create({
-	"title": "Linearize Tables",
+	"title": "Linearize tables",
 	"contexts": ["all", "page", "selection", "image", "link"],
 	"onclick" :  linearizeTables
 });
@@ -71,6 +66,18 @@ chrome.contextMenus.create({
 	"title": "Show focus order",
 	"contexts": ["all", "page", "selection", "image", "link"],
 	"onclick" :  showFocusOrder
+});
+
+chrome.contextMenus.create({
+	"title": "Enhanced Focus",
+	"contexts": ["all", "page", "selection", "image", "link"],
+	"onclick" :  enhancedFocus
+});
+
+chrome.contextMenus.create({
+	"title": "Darken",
+	"contexts": ["all", "page", "selection", "image", "link"],
+	"onclick" :  darken
 });
 
 function showARIA(info, tab) {
@@ -113,4 +120,8 @@ function showTitles(info, tab) {
 
 function showFocusOrder(info, tab) {
   chrome.tabs.sendMessage(tab.id, {msg:"showFocusOrder"}, function(response) { } );
+}
+
+function removeBackgroundImages(info, tab) {
+	chrome.tabs.insertCSS(null, {file:"removeBackgroundImages.css"} );  
 }
